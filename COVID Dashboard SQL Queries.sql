636c1c9358545fce11c1d4c0a07367c68fd0dd29
joshuaagbroko <161100739+joshuaagbroko@users.noreply.github.com>
2024-02-28 02:17:40 +0100

    Add files via upload

---
+++ COVID Dashboard SQL Queries.sql
@@ -0,0 +1,47 @@
+/*
+
+Queries used for COVID Dashboard Project
+
+*/
+
+
+
+-- 1. Summary of Global Numbers
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
+-- 2. Total Death count by Location
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Percentage of Population Infected by Country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Percentage of Populaion Infected by Count and Date
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths$
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
